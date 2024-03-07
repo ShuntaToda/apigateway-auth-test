45fe025d8170e9134a26bedbf67dc3ccf8dcdb6b
ShuntaToda <todashuntawork@gmail.com>
2024-03-07 10:56:25 +0900

    トークン取得完了

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&state=xyz&scope=openid&redirect_uri=http://127.0.0.1:3000`
+  const handleLogin = async () => {
+    window.location.href = authUrl
+  }
+
+  useEffect(() => {
+    console.log(window.location)
+    const params = new URLSearchParams(window.location.search); // ハッシュの最初の#を取り除く
+    const code = params.get('code');
+
+    console.log(code)
+    getToken(code)
+  }, []);
+
+
+  const getToken = async (code) => {
+    try {
+      const params = {
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        client_secret: process.env.REACT_APP_GOOGLE_SECRET,
+        redirect_uri: "http://127.0.0.1:3000",
+        grant_type: "authorization_code",
+        code: code
+      }
+      const response = await fetch("https://www.googleapis.com/oauth2/v4/token/", {
+        method: 'POST',
+        body: JSON.stringify(params)
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('API Request failed:', error);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleLogin}>login</button>
     </div>
   );
 }
