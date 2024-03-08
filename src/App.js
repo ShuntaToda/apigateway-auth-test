272db5f537629879b7eb280bc4ad406672b86046
ShuntaToda <todashuntawork@gmail.com>
2024-03-08 12:13:07 +0900

    コード整理

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&state=xyz&scope=openid&redirect_uri=http://127.0.0.1:3000`
+  const [authInfo, setAuthInfo] = useState({})
   const handleLogin = async () => {
-    window.location.href = authUrl
+    const params = {
+      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      redirect_uri: "http://127.0.0.1:3000",
+      scope: "openid",
+      response_type: "code",
+      state: "xyz"
+    }
+    const query = new URLSearchParams(params).toString()
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?${query}`
   }
 
   useEffect(() => {
     console.log(window.location)
-    const params = new URLSearchParams(window.location.search); // ハッシュの最初の#を取り除く
+    const params = new URLSearchParams(window.location.search);
     const code = params.get('code');
-
-    console.log(code)
-    getToken(code)
+    const getAuthInfo = async () => {
+      const responseAuthInfo = await requestAuthInfo(code)
+      console.log(responseAuthInfo)
+      setAuthInfo(responseAuthInfo)
+    }
+    getAuthInfo()
   }, []);
 
+  useEffect(() => {
+    const getMessage = async () => {
+      const response = await requestMessage(authInfo)
+      console.log(response)
+    }
+    // console.log(Object.keys(authInfo).length === 0 ? 1 : 0)
 
-  const getToken = async (code) => {
+    if (Object.keys(authInfo).length === 0 ? 1 : 0) getMessage()
+  }, [authInfo])
+
+
+  const requestAuthInfo = async (code) => {
     try {
       const params = {
         client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
@@ -31,16 +53,35 @@ function App() {
         body: JSON.stringify(params)
       });
       const data = await response.json();
-      console.log(data);
+
+      return data
     } catch (error) {
       console.error('API Request failed:', error);
     }
   };
 
+  const requestMessage = async (authInfo) => {
+    try {
+      const response = await fetch("https://p2uj50alqh.execute-api.ap-northeast-1.amazonaws.com/poc-lambda-jwt01", {
+        method: 'GET',
+        headers: {
+          "Authorization": `Bearer ${authInfo.id_token}`,
+        }
+      });
+      const data = await response.json();
+      console.log(data);
+
+      return data
+    } catch (error) {
+      console.error('API Request failed:', error);
+    }
+  }
 
   return (
-    <div className="App">
-      <button onClick={handleLogin}>login</button>
+    <div className="App container">
+      <div className='mt-4 text-center'>
+        <button className='btn btn-primary' onClick={handleLogin}>login</button>
+      </div>
     </div>
   );
 }
